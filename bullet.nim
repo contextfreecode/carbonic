e7edef50b849976112015e8fb83c649b4c101243
Tom <tom@contextfree.info>
2022-08-06 04:14:03 -0700

    Make dynamics world wrongly

--- bullet.nim
+++ bullet.nim
@@ -4,8 +4,42 @@ const
 type
     Vector3* {.header: bullet, importcpp: "btVector3".} = object
     Transform* {.header: bullet, importcpp: "btTransform".} = object
+    CollisionConfiguration* {.
+        header: bullet,
+        importcpp: "btCollisionConfiguration".} = object of RootObj
+    DefaultCollisionConfiguration* {.
+            header: bullet,
+            importcpp: "btDefaultCollisionConfiguration".} =
+        object of CollisionConfiguration
+    CollisionDispatcher* {.
+        header: bullet,
+        importcpp: "btCollisionDispatcher".} = object
+    DbvtBroadphase* {.header: bullet, importcpp: "btDbvtBroadphase".} = object
+    SequentialImpulseConstraintSolver* {.
+        header: bullet,
+        importcpp: "btSequentialImpulseConstraintSolver".} = object
+    DiscreteDynamicsWorld* {.
+        header: bullet,
+        importcpp: "btDiscreteDynamicsWorld".} = object
 
-func newVector3*(x: float32, y: float32, z: float32): Vector3 {.
+func makeCollisionDispatcher*(
+    collisionConfiguration: ptr CollisionConfiguration,
+): CollisionDispatcher {.
+    header: bullet,
+    importcpp: "btCollisionDispatcher(@)",
+    constructor.}
+
+func makeDiscreteDynamicsWorld*(
+    collisionConfiguration: ptr CollisionConfiguration,
+    dispatcher: ptr CollisionDispatcher,
+    overlappingPairCache: ptr DbvtBroadphase,
+    solver: ptr SequentialImpulseConstraintSolver,
+): DiscreteDynamicsWorld {.
+    header: bullet,
+    importcpp: "btDiscreteDynamicsWorld(@)",
+    constructor.}
+
+func makeVector3*(x: float32, y: float32, z: float32): Vector3 {.
     header: bullet,
     importcpp: "btVector3(@)",
     constructor.}
@@ -22,7 +56,7 @@ func y*(vector: Vector3): float32 {.
     header: bullet,
     importcpp: "#.getY()".}
 
-func newTransform*(): Transform {.
+func makeTransform*(): Transform {.
     header: bullet,
     importcpp: "btTransform()",
     constructor.}
--- bullet_demo.nim
+++ bullet_demo.nim
@@ -1,13 +1,42 @@
 import bullet
 import std/strformat
 
+type
+    DynamicsWorldStore = object
+        collisionConfiguration: DefaultCollisionConfiguration
+        dispatcher: CollisionDispatcher
+        overlappingPairCache: DbvtBroadphase
+        solver: SequentialImpulseConstraintSolver
+        world: DiscreteDynamicsWorld
+
+func makeDynamicsWorldStore(): DynamicsWorldStore =
+    let
+        collisionConfiguration = DefaultCollisionConfiguration()
+        dispatcher = makeCollisionDispatcher(unsafeAddr collisionConfiguration)
+        overlappingPairCache = DbvtBroadphase()
+        solver = SequentialImpulseConstraintSolver()
+    DynamicsWorldStore(
+        collisionConfiguration: collisionConfiguration,
+        dispatcher: dispatcher,
+        overlappingPairCache: overlappingPairCache,
+        solver: solver,
+        world: makeDiscreteDynamicsWorld(
+            unsafeAddr collisionConfiguration,
+            unsafeAddr dispatcher,
+            unsafeAddr overlappingPairCache,
+            unsafeAddr solver,
+        )
+    )
+
 proc main() =
-    let transform = newTransform()
+    let dynamicsWorldStore = makeDynamicsWorldStore()
+    discard dynamicsWorldStore.world
+    let transform = makeTransform()
     transform.setIdentity()
     echo(fmt"{transform.origin.x}")
     transform.origin.x = 1.5
     echo(fmt"{transform.origin.x}")
-    let origin = newVector3(1, 2, 3)
+    let origin = makeVector3(1, 2, 3)
     echo(fmt"{origin.y}")
 
 main()
