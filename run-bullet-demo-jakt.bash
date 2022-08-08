1ae68df3cea365fed7ffba6a4ea5f2df9bec604c
Tom <tom@contextfree.info>
2022-08-08 16:44:28 -0700

    Start jakt

---
+++ run-bullet-demo-jakt.bash
@@ -0,0 +1,11 @@
+BULLET=$HOME/projects/bullet3
+BULLET_LIB=$BULLET/build/src
+JAKT=$HOME/projects/jakt
+$JAKT/build/jakt -C clang++-12 -R $JAKT/runtime \
+    -I $BULLET/src \
+    -L $BULLET_LIB/BulletCollision \
+    -L $BULLET_LIB/BulletDynamics \
+    -L $BULLET_LIB/LinearMath \
+    -l BulletDynamics -l BulletCollision -l LinearMath \
+    -b bullet-demo.jakt && \
+valgrind ./build/bullet-demo
