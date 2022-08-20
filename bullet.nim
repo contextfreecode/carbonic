43c2a83a157a7bebc20de2362c17851c0d4b51fe
Tom <tom@contextfree.info>
2022-08-19 19:30:59 -0700

    Make progress on nim

--- bullet.nim
+++ bullet.nim
@@ -2,25 +2,50 @@ const
     bullet = "btBulletDynamicsCommon.h"
 
 type
-    Vector3* {.header: bullet, importcpp: "btVector3".} = object
-    Transform* {.header: bullet, importcpp: "btTransform".} = object
+    AlignedObjectArray*[T] {.
+        header: bullet,
+        importcpp: "btAlignedObjectArray".} = object
+
     CollisionConfiguration* {.
         header: bullet,
         importcpp: "btCollisionConfiguration".} = object of RootObj
+
+    CollisionDispatcher* {.
+        header: bullet,
+        importcpp: "btCollisionDispatcher".} = object
+
+    CollisionObject* {.
+        header: bullet,
+        importcpp: "btCollisionObject".} = object
+
+    DbvtBroadphase* {.header: bullet, importcpp: "btDbvtBroadphase".} = object
+
     DefaultCollisionConfiguration* {.
             header: bullet,
             importcpp: "btDefaultCollisionConfiguration".} =
         object of CollisionConfiguration
-    CollisionDispatcher* {.
-        header: bullet,
-        importcpp: "btCollisionDispatcher".} = object
-    DbvtBroadphase* {.header: bullet, importcpp: "btDbvtBroadphase".} = object
-    SequentialImpulseConstraintSolver* {.
-        header: bullet,
-        importcpp: "btSequentialImpulseConstraintSolver".} = object
+
     DiscreteDynamicsWorld* {.
         header: bullet,
         importcpp: "btDiscreteDynamicsWorld".} = object
+
+    MotionState* {.
+        header: bullet,
+        importcpp: "btMotionState".} = object
+
+    RigidBody* {.
+        header: bullet,
+        importcpp: "btRigidBody".} = object
+
+    SequentialImpulseConstraintSolver* {.
+        header: bullet,
+        importcpp: "btSequentialImpulseConstraintSolver".} = object
+
+    Transform* {.header: bullet, importcpp: "btTransform".} = object
+
+    Vector3* {.header: bullet, importcpp: "btVector3".} = object
+
+proc cdelete*(obj: ptr auto) {.importcpp: "delete #".}
 
 func makeCollisionDispatcher*(
     collisionConfiguration: ptr CollisionConfiguration,
@@ -29,15 +54,53 @@ func makeCollisionDispatcher*(
     importcpp: "btCollisionDispatcher(@)",
     constructor.}
 
+proc `[]`*[T](this: var AlignedObjectArray[T], n: int): var T {.
+    header: bullet,
+    importcpp: "#[#]".}
+
+func makeDbvtBroadphase*(): DbvtBroadphase {.
+    header: bullet,
+    importcpp: "btDbvtBroadphase()",
+    constructor.}
+
+func makeDefaultCollisionConfiguration*(): DefaultCollisionConfiguration {.
+    header: bullet,
+    importcpp: "btDefaultCollisionConfiguration()",
+    constructor.}
+
 func makeDiscreteDynamicsWorld*(
-    collisionConfiguration: ptr CollisionConfiguration,
     dispatcher: ptr CollisionDispatcher,
     overlappingPairCache: ptr DbvtBroadphase,
     solver: ptr SequentialImpulseConstraintSolver,
+    collisionConfiguration: ptr CollisionConfiguration,
 ): DiscreteDynamicsWorld {.
     header: bullet,
     importcpp: "btDiscreteDynamicsWorld(@)",
     constructor.}
+
+func collisionObjectArray*(
+    world: DiscreteDynamicsWorld
+): var AlignedObjectArray[ptr CollisionObject] {.
+    header: bullet,
+    importcpp: "#.getCollisionObjectArray()".}
+
+func numCollisionObjects*(world: DiscreteDynamicsWorld): int {.
+    header: bullet,
+    importcpp: "#.getNumCollisionObjects()".}
+
+func removeCollisionObject*(
+    world: var DiscreteDynamicsWorld, obj: ptr CollisionObject
+) {.
+    header: bullet,
+    importcpp: "#.removeCollisionObject(#)".}
+
+proc rigidBodyUpcast*(obj: ptr CollisionObject): ptr RigidBody {.
+    header: bullet,
+    importcpp: "btRigidBody::upcast(@)".}
+
+proc motionState*(body: ptr RigidBody): ptr MotionState {.
+    header: bullet,
+    importcpp: "#.getMotionState()".}
 
 func makeVector3*(x: float32, y: float32, z: float32): Vector3 {.
     header: bullet,
--- bullet_demo.nim
+++ bullet_demo.nim
@@ -1,36 +1,58 @@
 import bullet
 import std/strformat
 
-type
-    DynamicsWorldStore = object
-        collisionConfiguration: DefaultCollisionConfiguration
-        dispatcher: CollisionDispatcher
-        overlappingPairCache: DbvtBroadphase
-        solver: SequentialImpulseConstraintSolver
-        world: DiscreteDynamicsWorld
+# type
+#     DynamicsWorldStore = object
+#         collisionConfiguration: DefaultCollisionConfiguration
+#         dispatcher: CollisionDispatcher
+#         overlappingPairCache: DbvtBroadphase
+#         solver: SequentialImpulseConstraintSolver
+#         world: DiscreteDynamicsWorld
 
-func makeDynamicsWorldStore(): DynamicsWorldStore =
-    let
-        collisionConfiguration = DefaultCollisionConfiguration()
+# func makeDynamicsWorldStore(): DynamicsWorldStore =
+#     let
+#         collisionConfiguration = DefaultCollisionConfiguration()
+#         dispatcher = makeCollisionDispatcher(unsafeAddr collisionConfiguration)
+#         overlappingPairCache = DbvtBroadphase()
+#         solver = SequentialImpulseConstraintSolver()
+#     DynamicsWorldStore(
+#         collisionConfiguration: collisionConfiguration,
+#         dispatcher: dispatcher,
+#         overlappingPairCache: overlappingPairCache,
+#         solver: solver,
+#         world: makeDiscreteDynamicsWorld(
+#             unsafeAddr collisionConfiguration,
+#             unsafeAddr dispatcher,
+#             unsafeAddr overlappingPairCache,
+#             unsafeAddr solver,
+#         )
+#     )
+
+proc cleanDynamicsWorld(world: var DiscreteDynamicsWorld) =
+    for i in countdown(world.numCollisionObjects - 1, 0):
+        var
+            obj = world.collisionObjectArray()[i]
+            body = rigidBodyUpcast(obj)
+        if body != nil and body.motionState != nil:
+            body.motionState.cdelete
+        world.removeCollisionObject(obj)
+        obj.cdelete
+
+proc main() =
+    # let dynamicsWorldStore = makeDynamicsWorldStore()
+    # discard dynamicsWorldStore.world
+    var
+        collisionConfiguration = makeDefaultCollisionConfiguration()
         dispatcher = makeCollisionDispatcher(unsafeAddr collisionConfiguration)
-        overlappingPairCache = DbvtBroadphase()
+        overlappingPairCache = makeDbvtBroadphase()
         solver = SequentialImpulseConstraintSolver()
-    DynamicsWorldStore(
-        collisionConfiguration: collisionConfiguration,
-        dispatcher: dispatcher,
-        overlappingPairCache: overlappingPairCache,
-        solver: solver,
-        world: makeDiscreteDynamicsWorld(
-            unsafeAddr collisionConfiguration,
+        world = makeDiscreteDynamicsWorld(
             unsafeAddr dispatcher,
             unsafeAddr overlappingPairCache,
             unsafeAddr solver,
+            unsafeAddr collisionConfiguration,
         )
-    )
-
-proc main() =
-    let dynamicsWorldStore = makeDynamicsWorldStore()
-    discard dynamicsWorldStore.world
+    defer: cleanDynamicsWorld(world)
     let transform = makeTransform()
     transform.setIdentity()
     echo(fmt"{transform.origin.x}")
