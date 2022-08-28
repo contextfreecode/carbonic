ca024bb15da3506f0d5223b5951900e0b4180492
Tom <tom@contextfree.info>
2022-08-28 06:23:18 -0700

    Indent by 2 in nim

--- bullet.nim
+++ bullet.nim
@@ -1,179 +1,179 @@
 const
-    bullet = "btBulletDynamicsCommon.h"
+  bullet = "btBulletDynamicsCommon.h"
 
 type
-    AlignedObjectArray*[T] {.
-        header: bullet, importcpp: "btAlignedObjectArray".} = object
+  AlignedObjectArray*[T] {.
+    header: bullet, importcpp: "btAlignedObjectArray".} = object
 
-    CollisionConfiguration* {.
-        header: bullet,
-        importcpp: "btCollisionConfiguration".} = object of RootObj
+  CollisionConfiguration* {.
+    header: bullet,
+    importcpp: "btCollisionConfiguration".} = object of RootObj
 
-    CollisionDispatcher* {.
-        header: bullet, importcpp: "btCollisionDispatcher".} = object
+  CollisionDispatcher* {.
+    header: bullet, importcpp: "btCollisionDispatcher".} = object
 
-    CollisionObject* {.
-        header: bullet, importcpp: "btCollisionObject".} = object
+  CollisionObject* {.
+    header: bullet, importcpp: "btCollisionObject".} = object
 
-    CollisionShape* {.
-        header: bullet, importcpp: "btCollisionShape".} = object of RootObj
+  CollisionShape* {.
+    header: bullet, importcpp: "btCollisionShape".} = object of RootObj
 
-    BoxShape* {.
-        header: bullet, importcpp: "btBoxShape".} = object of CollisionShape
+  BoxShape* {.
+    header: bullet, importcpp: "btBoxShape".} = object of CollisionShape
 
-    SphereShape* {.
-        header: bullet,
-        importcpp: "btSphereShape".} = object of CollisionShape
+  SphereShape* {.
+    header: bullet,
+    importcpp: "btSphereShape".} = object of CollisionShape
 
-    DbvtBroadphase* {.header: bullet, importcpp: "btDbvtBroadphase".} = object
+  DbvtBroadphase* {.header: bullet, importcpp: "btDbvtBroadphase".} = object
 
-    DefaultCollisionConfiguration* {.
-            header: bullet, importcpp: "btDefaultCollisionConfiguration".} =
-        object of CollisionConfiguration
+  DefaultCollisionConfiguration* {.
+      header: bullet, importcpp: "btDefaultCollisionConfiguration".} =
+    object of CollisionConfiguration
 
-    DiscreteDynamicsWorld* {.
-        header: bullet, importcpp: "btDiscreteDynamicsWorld".} = object
+  DiscreteDynamicsWorld* {.
+    header: bullet, importcpp: "btDiscreteDynamicsWorld".} = object
 
-    MotionState* {.
-        header: bullet, importcpp: "btMotionState".} = object of RootObj
+  MotionState* {.
+    header: bullet, importcpp: "btMotionState".} = object of RootObj
 
-    DefaultMotionState* {.
-        header: bullet,
-        importcpp: "btDefaultMotionState".} = object of MotionState
+  DefaultMotionState* {.
+    header: bullet,
+    importcpp: "btDefaultMotionState".} = object of MotionState
 
-    RigidBody* {.
-        header: bullet, importcpp: "btRigidBody".} = object
+  RigidBody* {.
+    header: bullet, importcpp: "btRigidBody".} = object
 
-    RigidBodyConstructionInfo* {.
-            header: bullet,
-            importcpp: "btRigidBody::btRigidBodyConstructionInfo"
-        .} = object
+  RigidBodyConstructionInfo* {.
+      header: bullet,
+      importcpp: "btRigidBody::btRigidBodyConstructionInfo"
+    .} = object
 
-    SequentialImpulseConstraintSolver* {.
-        header: bullet,
-        importcpp: "btSequentialImpulseConstraintSolver".} = object
+  SequentialImpulseConstraintSolver* {.
+    header: bullet,
+    importcpp: "btSequentialImpulseConstraintSolver".} = object
 
-    Transform* {.header: bullet, importcpp: "btTransform".} = object
+  Transform* {.header: bullet, importcpp: "btTransform".} = object
 
-    Vector3* {.header: bullet, importcpp: "btVector3".} = object
+  Vector3* {.header: bullet, importcpp: "btVector3".} = object
 
 proc `[]`*[T](this: var AlignedObjectArray[T], n: int): var T {.
-    header: bullet,
-    importcpp: "#[#]".}
+  header: bullet,
+  importcpp: "#[#]".}
 
 func makeBoxShape*(halfExtents: Vector3): BoxShape {.
-    header: bullet, importcpp: "btBoxShape(@)", constructor.}
+  header: bullet, importcpp: "btBoxShape(@)", constructor.}
 
 func makeCollisionDispatcher*(
-    collisionConfiguration: ptr CollisionConfiguration,
+  collisionConfiguration: ptr CollisionConfiguration,
 ): CollisionDispatcher {.
-    header: bullet,
-    importcpp: "btCollisionDispatcher(@)",
-    constructor.}
+  header: bullet,
+  importcpp: "btCollisionDispatcher(@)",
+  constructor.}
 
 func worldTransform*(obj: ptr CollisionObject): Transform {.
-    header: bullet, importcpp: "#.getWorldTransform()".}
+  header: bullet, importcpp: "#.getWorldTransform()".}
 
 proc calculateLocalInertia*(
-    collisionShape: CollisionShape, mass: float32, localInertia: var Vector3
+  collisionShape: CollisionShape, mass: float32, localInertia: var Vector3
 ) {.header: bullet, importcpp: "#.calculateLocalInertia(@)".}
 
 func makeDbvtBroadphase*(): DbvtBroadphase {.
-    header: bullet,
-    importcpp: "btDbvtBroadphase()",
-    constructor.}
+  header: bullet,
+  importcpp: "btDbvtBroadphase()",
+  constructor.}
 
 func makeDefaultCollisionConfiguration*(): DefaultCollisionConfiguration {.
-    header: bullet,
-    importcpp: "btDefaultCollisionConfiguration()",
-    constructor.}
+  header: bullet,
+  importcpp: "btDefaultCollisionConfiguration()",
+  constructor.}
 
 func makeDiscreteDynamicsWorld*(
-    dispatcher: ptr CollisionDispatcher,
-    overlappingPairCache: ptr DbvtBroadphase,
-    solver: ptr SequentialImpulseConstraintSolver,
-    collisionConfiguration: ptr CollisionConfiguration,
+  dispatcher: ptr CollisionDispatcher,
+  overlappingPairCache: ptr DbvtBroadphase,
+  solver: ptr SequentialImpulseConstraintSolver,
+  collisionConfiguration: ptr CollisionConfiguration,
 ): DiscreteDynamicsWorld {.
-    header: bullet,
-    importcpp: "btDiscreteDynamicsWorld(@)",
-    constructor.}
+  header: bullet,
+  importcpp: "btDiscreteDynamicsWorld(@)",
+  constructor.}
 
 func addRigidBody*(
-    world: var DiscreteDynamicsWorld, body: ptr RigidBody
+  world: var DiscreteDynamicsWorld, body: ptr RigidBody
 ) {.header: bullet, importcpp: "#.addRigidBody(#)".}
 
 func collisionObjectArray*(
-    world: DiscreteDynamicsWorld
+  world: DiscreteDynamicsWorld
 ): var AlignedObjectArray[ptr CollisionObject] {.
-    header: bullet,
-    importcpp: "#.getCollisionObjectArray()".}
+  header: bullet,
+  importcpp: "#.getCollisionObjectArray()".}
 
 func numCollisionObjects*(world: DiscreteDynamicsWorld): int {.
-    header: bullet,
-    importcpp: "#.getNumCollisionObjects()".}
+  header: bullet,
+  importcpp: "#.getNumCollisionObjects()".}
 
 proc removeCollisionObject*(
-    world: var DiscreteDynamicsWorld, obj: ptr CollisionObject
+  world: var DiscreteDynamicsWorld, obj: ptr CollisionObject
 ) {.
-    header: bullet,
-    importcpp: "#.removeCollisionObject(#)".}
+  header: bullet,
+  importcpp: "#.removeCollisionObject(#)".}
 
 proc stepSimulation*(
-    world: var DiscreteDynamicsWorld, timeStep: float32, maxSubSteps: int
+  world: var DiscreteDynamicsWorld, timeStep: float32, maxSubSteps: int
 ) {.header: bullet, importcpp: "#.stepSimulation(@)".}
 
 func makeDefaultMotionState*(transform: Transform): DefaultMotionState {.
-    header: bullet, importcpp: "btDefaultMotionState(@)", constructor.}
+  header: bullet, importcpp: "btDefaultMotionState(@)", constructor.}
 
 proc getWorldTransform*(
-    motionState: ptr MotionState, transform: var Transform
+  motionState: ptr MotionState, transform: var Transform
 ) {.header: bullet, importcpp: "#.getWorldTransform(@)".}
 
 func makeRigidBody*(info: RigidBodyConstructionInfo): RigidBody {.
-    header: bullet, importcpp: "btRigidBody(@)", constructor.}
+  header: bullet, importcpp: "btRigidBody(@)", constructor.}
 
 proc rigidBodyUpcast*(obj: ptr CollisionObject): ptr RigidBody {.
-    header: bullet,
-    importcpp: "btRigidBody::upcast(@)".}
+  header: bullet,
+  importcpp: "btRigidBody::upcast(@)".}
 
 proc motionState*(body: ptr RigidBody): ptr MotionState {.
-    header: bullet,
-    importcpp: "#.getMotionState()".}
+  header: bullet,
+  importcpp: "#.getMotionState()".}
 
 func makeRigidBodyConstructionInfo*(
-    mass: float32,
-    motionState: ptr MotionState,
-    shape: ptr CollisionShape,
-    localInertia: Vector3,
+  mass: float32,
+  motionState: ptr MotionState,
+  shape: ptr CollisionShape,
+  localInertia: Vector3,
 ): RigidBodyConstructionInfo {.
-    header: bullet,
-    importcpp: "btRigidBody::btRigidBodyConstructionInfo(@)",
-    constructor.}
+  header: bullet,
+  importcpp: "btRigidBody::btRigidBodyConstructionInfo(@)",
+  constructor.}
 
 func makeSphereShape*(radius: float32): SphereShape {.
-    header: bullet, importcpp: "btSphereShape(@)", constructor.}
+  header: bullet, importcpp: "btSphereShape(@)", constructor.}
 
 func makeTransform*(): Transform {.
-    header: bullet,
-    importcpp: "btTransform()",
-    constructor.}
+  header: bullet,
+  importcpp: "btTransform()",
+  constructor.}
 
 proc origin*(transform: Transform): var Vector3 {.
-    header: bullet,
-    importcpp: "#.getOrigin()".}
+  header: bullet,
+  importcpp: "#.getOrigin()".}
 
 proc `origin=`*(transform: var Transform, origin: Vector3) {.
-    header: bullet,
-    importcpp: "#.setOrigin(#)".}
+  header: bullet,
+  importcpp: "#.setOrigin(#)".}
 
 proc setIdentity*(transform: Transform) {.
-    header: bullet,
-    importcpp: "#.setIdentity()".}
+  header: bullet,
+  importcpp: "#.setIdentity()".}
 
 func makeVector3*(x: float32, y: float32, z: float32): Vector3 {.
-    header: bullet,
-    importcpp: "btVector3(@)",
-    constructor.}
+  header: bullet,
+  importcpp: "btVector3(@)",
+  constructor.}
 
 func x*(vector: Vector3): float32 {.header: bullet, importcpp: "#.getX()".}
 
--- bullet_demo.nim
+++ bullet_demo.nim
@@ -2,103 +2,103 @@ import bullet
 import std/strformat
 
 # type
-#     DynamicsWorldStore = object
-#         collisionConfiguration: DefaultCollisionConfiguration
-#         dispatcher: CollisionDispatcher
-#         overlappingPairCache: DbvtBroadphase
-#         solver: SequentialImpulseConstraintSolver
-#         world: DiscreteDynamicsWorld
+#   DynamicsWorldStore = object
+#     collisionConfiguration: DefaultCollisionConfiguration
+#     dispatcher: CollisionDispatcher
+#     overlappingPairCache: DbvtBroadphase
+#     solver: SequentialImpulseConstraintSolver
+#     world: DiscreteDynamicsWorld
 
 # func makeDynamicsWorldStore(): DynamicsWorldStore =
-#     let
-#         collisionConfiguration = DefaultCollisionConfiguration()
-#         dispatcher = makeCollisionDispatcher(unsafeAddr collisionConfiguration)
-#         overlappingPairCache = DbvtBroadphase()
-#         solver = SequentialImpulseConstraintSolver()
-#     DynamicsWorldStore(
-#         collisionConfiguration: collisionConfiguration,
-#         dispatcher: dispatcher,
-#         overlappingPairCache: overlappingPairCache,
-#         solver: solver,
-#         world: makeDiscreteDynamicsWorld(
-#             unsafeAddr dispatcher,
-#             unsafeAddr overlappingPairCache,
-#             unsafeAddr solver,
-#             unsafeAddr collisionConfiguration,
-#         )
+#   let
+#     collisionConfiguration = DefaultCollisionConfiguration()
+#     dispatcher = makeCollisionDispatcher(unsafeAddr collisionConfiguration)
+#     overlappingPairCache = DbvtBroadphase()
+#     solver = SequentialImpulseConstraintSolver()
+#   DynamicsWorldStore(
+#     collisionConfiguration: collisionConfiguration,
+#     dispatcher: dispatcher,
+#     overlappingPairCache: overlappingPairCache,
+#     solver: solver,
+#     world: makeDiscreteDynamicsWorld(
+#       unsafeAddr dispatcher,
+#       unsafeAddr overlappingPairCache,
+#       unsafeAddr solver,
+#       unsafeAddr collisionConfiguration,
 #     )
+#   )
 
 proc delete*(obj: ptr auto) {.importcpp: "delete #".}
 
 proc new*[T](t: T): ptr T {.importcpp: "(new '*0#@)".}
 
 proc cleanDynamicsWorld(world: var DiscreteDynamicsWorld) =
-    for i in countdown(world.numCollisionObjects - 1, 0):
-        var
-            obj = world.collisionObjectArray[i]
-            body = rigidBodyUpcast(obj)
-        if body != nil and body.motionState != nil:
-            body.motionState.delete
-        world.removeCollisionObject(obj)
-        obj.delete
+  for i in countdown(world.numCollisionObjects - 1, 0):
+    var
+      obj = world.collisionObjectArray[i]
+      body = rigidBodyUpcast(obj)
+    if body != nil and body.motionState != nil:
+      body.motionState.delete
+    world.removeCollisionObject(obj)
+    obj.delete
 
 proc initBody(
-    mass: float32, origin: Vector3, shape: var CollisionShape
+  mass: float32, origin: Vector3, shape: var CollisionShape
 ): ptr RigidBody =
-    var
-        localInertia = makeVector3(0, 0, 0)
-        transform = makeTransform()
-    transform.setIdentity()
-    transform.origin = origin
-    let isDynamic = mass != 0
-    if isDynamic:
-        shape.calculateLocalInertia(mass = mass, localInertia = localInertia)
-    var motionState = makeDefaultMotionState(transform).new
-    let rbInfo = makeRigidBodyConstructionInfo(
-        mass = mass,
-        motionState = motionState,
-        shape = unsafeAddr shape,
-        localInertia = localInertia,
-    )
-    makeRigidBody(rbInfo).new
+  var
+    localInertia = makeVector3(0, 0, 0)
+    transform = makeTransform()
+  transform.setIdentity()
+  transform.origin = origin
+  let isDynamic = mass != 0
+  if isDynamic:
+    shape.calculateLocalInertia(mass = mass, localInertia = localInertia)
+  var motionState = makeDefaultMotionState(transform).new
+  let rbInfo = makeRigidBodyConstructionInfo(
+    mass = mass,
+    motionState = motionState,
+    shape = unsafeAddr shape,
+    localInertia = localInertia,
+  )
+  makeRigidBody(rbInfo).new
 
 proc printAll(world: var DiscreteDynamicsWorld) =
-    for i in 0..<world.numCollisionObjects:
-        let
-            obj = world.collisionObjectArray[i]
-            body = rigidBodyUpcast(obj)
-        var transform = makeTransform()
-        if body != nil and body.motionState != nil:
-            body.motionState.getWorldTransform(transform)
-        else:
-            transform = obj.worldTransform
-        let origin = transform.origin
-        echo(fmt"world pos object {i} = {origin.x} {origin.y} {origin.z}")
+  for i in 0..<world.numCollisionObjects:
+    let
+      obj = world.collisionObjectArray[i]
+      body = rigidBodyUpcast(obj)
+    var transform = makeTransform()
+    if body != nil and body.motionState != nil:
+      body.motionState.getWorldTransform(transform)
+    else:
+      transform = obj.worldTransform
+    let origin = transform.origin
+    echo(fmt"world pos object {i} = {origin.x} {origin.y} {origin.z}")
 
 proc main() =
-    var
-        collisionConfiguration = makeDefaultCollisionConfiguration()
-        dispatcher = makeCollisionDispatcher(unsafeAddr collisionConfiguration)
-        overlappingPairCache = makeDbvtBroadphase()
-        solver = SequentialImpulseConstraintSolver()
-        world = makeDiscreteDynamicsWorld(
-            dispatcher = unsafeAddr dispatcher,
-            overlappingPairCache = unsafeAddr overlappingPairCache,
-            solver = unsafeAddr solver,
-            collisionConfiguration = unsafeAddr collisionConfiguration,
-        )
-    defer: cleanDynamicsWorld(world)
-    var
-        groundShape = makeBoxShape(makeVector3(50, 50, 50))
-        sphereShape = makeSphereShape(1)
-    world.addRigidBody(
-        initBody(mass = 0, origin = makeVector3(0, -56, 0), shape = groundShape)
+  var
+    collisionConfiguration = makeDefaultCollisionConfiguration()
+    dispatcher = makeCollisionDispatcher(unsafeAddr collisionConfiguration)
+    overlappingPairCache = makeDbvtBroadphase()
+    solver = SequentialImpulseConstraintSolver()
+    world = makeDiscreteDynamicsWorld(
+      dispatcher = unsafeAddr dispatcher,
+      overlappingPairCache = unsafeAddr overlappingPairCache,
+      solver = unsafeAddr solver,
+      collisionConfiguration = unsafeAddr collisionConfiguration,
     )
-    world.addRigidBody(
-        initBody(mass = 1, origin = makeVector3(2, 10, 0), shape = sphereShape)
-    )
-    for i in 1..150:
-        world.stepSimulation(1.0 / 60.0, 10)
-        printAll(world)
+  defer: cleanDynamicsWorld(world)
+  var
+    groundShape = makeBoxShape(makeVector3(50, 50, 50))
+    sphereShape = makeSphereShape(1)
+  world.addRigidBody(
+    initBody(mass = 0, origin = makeVector3(0, -56, 0), shape = groundShape)
+  )
+  world.addRigidBody(
+    initBody(mass = 1, origin = makeVector3(2, 10, 0), shape = sphereShape)
+  )
+  for i in 1..150:
+    world.stepSimulation(1.0 / 60.0, 10)
+    printAll(world)
 
 main()
